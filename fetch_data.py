314a5e69aef5ce20df8fefad9c6e621835e32315
chunyo <r04922108@csie.ntu.edu.tw>
2018-03-20 22:58:18 +0800

    add: fetch from features to function get_stock_pd

--- fetch_data.py
+++ fetch_data.py
@@ -2,7 +2,9 @@ from twstock import Stock
 import pandas as pd
 from pandas import Series, DataFrame
 
-def get_stock_pd(stock_no = "0050"):
+def get_stock_pd(stock_no="0050", fetch_from=None):
     S = Stock(stock_no)
+    if fetch_from:
+        S.fetch_from(fetch_from[0], fetch_data[1])
     keys = list(S.data[0]._fields)
     return DataFrame(S.data, columns=keys).set_index("date")
