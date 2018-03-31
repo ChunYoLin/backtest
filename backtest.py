856b80ce23ba289af3853a36cc726e7a26b0fcba
Chunyolin <r04922108@csie.ntu.edu.tw>
2018-03-31 13:55:29 +0800

    refactoring: make stock no be program argument

--- backtest.py
+++ backtest.py
@@ -1,11 +1,13 @@
 from fetch_data import get_stock_pd
 from strategy import basic_strategy
 import backtrader as bt
+import sys
+
 
 if __name__ == '__main__':
     cerebro = bt.Cerebro()
 
-    stock_pd = get_stock_pd('2454', fetch_from=(2017, 1))
+    stock_pd = get_stock_pd(sys.argv[1], fetch_from=(2017, 1))
     data = bt.feeds.PandasData(
             dataname=stock_pd, 
             volume='transaction'
