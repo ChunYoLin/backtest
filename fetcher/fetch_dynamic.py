c3e9669604cc38829ede1455e56b5f15744f3693
Chunyolin <r04922108@csie.ntu.edu.tw>
2018-04-15 23:34:49 +0800

    fix: eliminate redundant fetch of twstock

--- fetcher/fetch_dynamic.py
+++ fetcher/fetch_dynamic.py
@@ -9,8 +9,7 @@ from twstock import Stock
 
 
 database_path = "/home/chunyo/work/backtest/database/"
-def _get_twstock_online(stock_no="0050", fetch_from=None, save=False):
-    S = Stock(stock_no)
+def _get_twstock_online(S, fetch_from=None, save=False):
     if fetch_from:
         S.fetch_from(fetch_from[0], fetch_from[1])
     if save:
@@ -35,28 +34,29 @@ def get_twstock(stock_no="0050", fetch_from=None):
     if fetch_from == None:
         print("please specify the start date")
         sys.exit(0)
+    _S = Stock(stock_no)
     if os.path.isfile("{}/{}.csv".format(database_path, stock_no)):
         print("local data found!")
         S =  _get_twstock_local(stock_no)
         #  if data too old download it again
-        end_datetime = Stock(stock_no).date[-1]
+        end_datetime = _S.date[-1]
         local_end_datetime = S[-1].date
         if local_end_datetime != end_datetime:
             print("end date not found, re-fetch online...")
-            S = _get_twstock_online(stock_no, fetch_from, save=True)
+            S = _get_twstock_online(_S, fetch_from, save=True)
             return S
         #  if data start date mismatched dowload it again
-        start_day = Stock(stock_no).fetch(fetch_from[0], fetch_from[1])[0].date.day
+        start_day = _S.fetch(fetch_from[0], fetch_from[1])[0].date.day
         start_datetime = datetime(fetch_from[0], fetch_from[1], start_day)
         for idx, s in enumerate(S):
             if s.date == start_datetime:
                 return S[idx:]
         print("start date not found, re-fetch online...")
-        S = _get_twstock_online(stock_no, fetch_from, save=True)
+        S = _get_twstock_online(_S, fetch_from, save=True)
         return S
 
     else:
         print("no local data, fetch online...")
-        S = _get_twstock_online(stock_no, fetch_from, save=True)
+        S = _get_twstock_online(_S, fetch_from, save=True)
     return S
 
